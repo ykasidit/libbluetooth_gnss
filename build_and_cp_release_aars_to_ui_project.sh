798057084f6dc0e7abbf2fa7334a3d9b074c0298
Kasidit Yusuf <ykasidit@gmail.com>
2021-05-15 10:08:06 +0700

    add sh to build and cp aars to ../bluetooth_gnss project folder

---
+++ build_and_cp_release_aars_to_ui_project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+exit_if_failed() {
+    if [ $? -ne 0 ]; then
+	echo "ABORT: Previous step failed"
+	exit 1
+    fi
+}
+
+./gradlew assembleRelease
+exit_if_failed
+
+cp libbluetooth_gnss_service/build/outputs/aar/libbluetooth_gnss_service-release.aar ../bluetooth_gnss/android/libbluetooth_gnss_service/
+exit_if_failed
+
+cp libecodroidbluetooth/build/outputs/aar/libecodroidbluetooth-release.aar ../bluetooth_gnss/android/libecodroidbluetooth/
+exit_if_failed
+
+cp libecodroidgnss_parse/build/outputs/aar/libecodroidgnss_parse-release.aar ../bluetooth_gnss/android/libecodroidgnss_parse/
+exit_if_failed
+echo "copied all aars to ui project (../bluetooth_gnss)"
+echo SUCCESS
+
